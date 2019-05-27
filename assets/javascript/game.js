76a08731ed6f8045b267fdf8d0b5fd6d8153c7d2
Derek Goldstone <derekgoldstone@yahoo.com>
2019-05-26 17:37:14 -0700

    Created functions that display/update current score, target score, wins, and losses to dom based on value of their respective variables. Created function to check if target score has been reached by comparing currentScore to target score and alerting player if they have won or lost. Added refresh score functionality to character on click event to update score after their image has been selected and checkScore function to determine if game has been won or lost depending on the point value assigned to that character's click event.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,22 +29,63 @@ var intializeGame = () => {
     kevinDurant = generatePlayerNum();
     klayThompson = generatePlayerNum();
     draymondGreen = generatePlayerNum();
+    refreshScore();
+    refreshTargetScore();
+    updateWins();
 };
-//function that starts the game by assigning values to the target score and player variables
+//function that starts the game by assigning values to the target score and player variables and displays target score,current score, wins, and losses
+
+var refreshScore = () => {
+    $("#current-score").text("Current Score: " + currentScore);
+};
+//function that utilizes JQuery to display/update currentScore variable on DOM at #current-score div id and refreshes every time game is intialized
+
+var refreshTargetScore = () => {
+    $("#score").text("Target Score: " + target);
+};
+//function that utilizes JQuery to display/update targetScore variable on DOM at #score div id and refreshes every time game is initialized
+
+var updateWins = () => {
+    $("#wins").text("Wins: " + wins);
+    $("#losses").text("Losses: " + losses);
+};
+//function that utilizes JQuery to display/update wins/losses variables on DOM at their respective div id's
+
+var checkScore = () => {
+    if (currentScore > target) {
+        alert("You lose!");
+        losses++;
+        updateWins();
+    } else if (currentScore === target) {
+        alert("You win!");
+        wins++;
+        updateWins();
+    }
+};
+//function that determines if a player has won or lost depending on the comparison of the currentScore variable to the target number variable, player will be notified with alert depending on outcome and a win or loss will be incremented to the corresponding variable
+
 
 $("#SC").on("click", function() {
     currentScore += stephCurry;
+    refreshScore();
+    checkScore();
 });
 $("#KD").on("click", function() {
     currentScore += kevinDurant;
+    refreshScore();
+    checkScore();
 });
 $("#KT").on("click", function() {
     currentScore += klayThompson;
+    refreshScore();
+    checkScore();
 });
 $("#DG").on("click", function() {
     currentScore += draymondGreen;
+    refreshScore();
+    checkScore();
 });
-//key event listener functions which adds a players score value to currentScore variable on each click event
+//key event listener functions which adds a players score value to currentScore variable on each click event and determines if the target score has been reached or exceeded
 
 
 initializeGame();
